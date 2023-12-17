be84f4492f2dee6a50af69b50856a0e10ef46f6b
凌小云 <46366997+lingxyun@users.noreply.github.com>
2023-12-17 11:02:31 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
           万象千问 AI
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          让AI更懂你.
+          开启大脑集群,激发思维火花.你的智能助理,一直都在.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
